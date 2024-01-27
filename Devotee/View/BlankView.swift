c9731a34c71e6713055611634b8e292730f836cd
Pranav Kulkarni <pranav.kulkarni@sap.com>
2024-01-27 23:02:40 +0530

    Blur effect

---
+++ Devotee/View/BlankView.swift
@@ -0,0 +1,18 @@
+//
+//  BlankView.swift
+//  Devotee
+//
+//  Created by Kulkarni, Pranav on 27/01/24.
+//
+
+import SwiftUI
+
+struct BlankView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlankView()
+}
